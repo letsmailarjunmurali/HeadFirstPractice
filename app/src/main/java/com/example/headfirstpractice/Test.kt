dcc6a97389e271295266a374233b214454830733
Arjun <letsmailarjunmurali@gmail.com>
2022-06-14 18:27:43 +0530

    chapter 1 commit

--- app/src/main/java/com/example/headfirstpractice/Test.kt
+++ app/src/main/java/com/example/headfirstpractice/Test.kt
@@ -2,5 +2,39 @@ package com.example.headfirstpractice
 
 fun main(args: Array<String>) {
 
-    print("Hello")
+    // statements
+    var x=3
+    val name="Arjun"
+    x=x+3
+    println("X is $x")
+
+    //Do something again and again
+    var i=0
+    while (i<10){
+        i++
+        println("Index is $i")
+    }
+
+    //Do something under a condition (branching)
+    if(x==20){
+        println("x must be 20")
+    }else{
+        println("x is not 20")
+    }
+
+
+    var value=1
+    println("Before the loop $value")
+    while (value<4){
+        println("In the loop $value")
+        value++
+    }
+    println("After the loop $value")
+
+    //if as expression
+    val a=10
+    val b=20
+    val result = if (a < b) "A is less" else "B is less"
+    println("Result is $result")
+
 }
